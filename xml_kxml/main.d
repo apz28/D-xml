0237fb6a18dd2821bced2287d15385a31ce1e67e
apz28 <apz28@hotmail.com>
2017-08-24 23:41:45 -0400

    Add files via upload

---
+++ xml_kxml/main.d
@@ -0,0 +1,26 @@
+module main;
+
+import kxml.xml;
+import main_test;
+
+Object loadXml(string xml)
+{
+    auto doc = new XmlDocument();
+    doc.parse(xml);
+    return doc;
+}
+
+string saveXml(Object doc)
+{
+    return (cast(XmlDocument) doc).toString();
+}
+
+int main(string[] argv)
+{
+    TestOptions o = TestOptions(argv);
+
+    TestExecute t = TestExecute(&loadXml, null, &saveXml);
+    t.execute(o);
+
+    return 0;
+}
